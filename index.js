f4a370910816122ff045d2c37758850740c9e86a
JMwill <787505998@qq.com>
2017-06-14 20:01:11 +0800

    安装并使用superagent来进行网络请求

--- index.js
+++ index.js
@@ -2,20 +2,26 @@
 
 const co        = require('co');
 const prompt    = require('co-prompt');
+const request   = require('superagent');
 const program   = require('commander');
 
 program
     .arguments('<file>')
     .option('-u, --username <username>', 'The user to authenticate as')
     .option('-p, --password <password>', 'The user\'s password')
-    .action(function(file) {
+    .action(function (file) {
         co(function *() {
             let username = yield prompt('username: ');
-            let password = yield prompt('password: ');
-            console.log(
-                'user: %s pass: %s file: %s',
-                username, password, file
-            );
+            let password = yield prompt.password('password: ');
+            request
+                .post('https://api.bitbucket.org/2.0/snippets/')
+                .auth(username, password)
+                .attach('file', file)
+                .set('Accept', 'application/json')
+                .end(function (err, res) {
+                    let link = res.body.links.html.href;
+                    console.log('Snippet created: %s', link);
+                });
         });
     })
     .parse(process.argv)
