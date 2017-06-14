41ed740a78a4def7fe20b2e0f51d1617784bf3f6
JMwill <787505998@qq.com>
2017-06-14 09:32:27 +0800

    安装co以及co-prompt来使用生成器进行异步编程

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 #!/usr/bin/env node
 
-const program = require('commander');
+const co        = require('co');
+const prompt    = require('co-prompt');
+const program   = require('commander');
 
 program
     .arguments('<file>')
     .option('-u, --username <username>', 'The user to authenticate as')
     .option('-p, --password <password>', 'The user\'s password')
     .action(function(file) {
-        console.log(
-            'user: %s pass: %s file: %s',
-            program.username, program.password, file
-        );
+        co(function *() {
+            let username = yield prompt('username: ');
+            let password = yield prompt('password: ');
+            console.log(
+                'user: %s pass: %s file: %s',
+                username, password, file
+            );
+        });
     })
     .parse(process.argv)
