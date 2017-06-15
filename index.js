a5cb2a372800e8562c12a85bec1a38ec314a9430
JMwill <787505998@qq.com>
2017-06-15 09:52:07 +0800

    添加progress模块并完成上传进度条功能

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
-const co        = require('co');
-const prompt    = require('co-prompt');
-const request   = require('superagent');
-const program   = require('commander');
-const chalk     = require('chalk');
+const fs            = require('fs');
+const co            = require('co');
+const prompt        = require('co-prompt');
+const request       = require('superagent');
+const program       = require('commander');
+const chalk         = require('chalk');
+const ProgressBar   = require('progress');
 
 program
     .arguments('<file>')
@@ -14,10 +16,24 @@ program
         co(function *() {
             let username = yield prompt('username: ');
             let password = yield prompt.password('password: ');
+
+            let fileSize = fs.statSync(file).size;
+            let fileStream = fs.createReadStream(file);
+            let barOpts = {
+                width: 20,
+                total: fileSize,
+                clear: true
+            };
+
+            let bar = new ProgressBar('uploading [:bar] :percent :etas', barOpts);
+
+            fileStream.on('data', chunk => {
+                bar.tick(chunk.length);
+            });
             request
                 .post('https://api.bitbucket.org/2.0/snippets/')
                 .auth(username, password)
-                .attach('file', file)
+                .attach('file', fileStream)
                 .set('Accept', 'application/json')
                 .end(function (err, res) {
                     if (!err && res.ok) {
