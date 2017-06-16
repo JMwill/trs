81dab511cd4dc5724a62b9ec887572ee0a2b31cb
JMwill <787505998@qq.com>
2017-06-16 13:01:16 +0800

    实现检测语言类型的函数

--- index.js
+++ index.js
@@ -8,52 +8,23 @@ const program       = require('commander');
 const chalk         = require('chalk');
 const ProgressBar   = require('progress');
 
-program
-    .arguments('<file>')
-    .option('-u, --username <username>', 'The user to authenticate as')
-    .option('-p, --password <password>', 'The user\'s password')
-    .action(function (file) {
-        co(function *() {
-            let username = yield prompt('username: ');
-            let password = yield prompt.password('password: ');
-
-            let fileSize = fs.statSync(file).size;
-            let fileStream = fs.createReadStream(file);
-            let barOpts = {
-                width: 20,
-                total: fileSize,
-                clear: true
-            };
-
-            let bar = new ProgressBar('uploading [:bar] :percent :etas', barOpts);
-
-            fileStream.on('data', chunk => {
-                bar.tick(chunk.length);
+function detectlang(detectword) {
+    return new Promise((resolve, reject) => {
+        request
+            .post('http://fanyi.baidu.com/langdetect')
+            .set('Content-Type', 'application/json')
+            .type('form')
+            .send({query: detectword})
+            .end((err, res) => {
+                if (err) { return reject(err); }
+                try {
+                    if (res.body.msg === 'success') {
+                        resolve(res.body.lan)
+                    }
+                    return res.body.lan;
+                } catch(err) {
+                    reject(err);
+                }
             });
-            request
-                .post('https://api.bitbucket.org/2.0/snippets/')
-                .auth(username, password)
-                .attach('file', fileStream)
-                .set('Accept', 'application/json')
-                .end(function (err, res) {
-                    if (!err && res.ok) {
-                        let link = res.body.links.html.href;
-                        console.log(chalk.bold.cyan('Snippet created: ') + link);
-                        process.exit(0);
-                    }
-
-                    let errMessage;
-                    if (res && res.status === 401) {
-                        errMessage = 'Authentication failed Bad username/password?';
-                    } else if (err) {
-                        errMessage = err;
-                    } else {
-                        errMessage = res.text;
-                    }
-                    console.error();
-                    console.log(chalk.red(errMessage));
-                    process.exit(1);
-                });
-        });
-    })
-    .parse(process.argv)
+    });
+}
