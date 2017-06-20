6d6b744eca30e4dee171de5cc1afee292bba3fb1
JMwill <787505998@qq.com>
2017-06-20 17:19:26 +0800

    安装eslint并对代码不规范的地方进行修整

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,7 +1,85 @@
-const os            = require('os');
-const request       = require('superagent');
-const boxen         = require('boxen');
-const logUpdate     = require('log-update');
+const os = require('os');
+const request = require('superagent');
+const boxen = require('boxen');
+const logUpdate = require('log-update');
+
+function reqTo(url, opt) {
+    return request
+        .post(url)
+        .set('Content-Type', 'application/json')
+        .type('form')
+        .send(opt);
+}
+
+function detectlang(detectword) {
+    return new Promise((resolve, reject) => {
+        reqTo('http://fanyi.baidu.com/langdetect', { query: detectword })
+            .end((err, res) => {
+                if (err) { reject(err); }
+                try {
+                    resolve(res.body.lan);
+                } catch (catErr) {
+                    reject(catErr);
+                }
+            });
+    });
+}
+
+function suggestTrans(needSubWords) {
+    return new Promise((resolve, reject) => {
+        reqTo('http://fanyi.baidu.com/sug', { kw: needSubWords })
+            .end((err, res) => {
+                if (err) { reject(err); }
+                try {
+                    resolve(res.body.data);
+                } catch (catErr) {
+                    reject(catErr);
+                }
+            });
+    });
+}
+
+function translate(reqOpt) {
+    return new Promise((resolve, reject) => {
+        reqTo('http://fanyi.baidu.com/v2transapi', reqOpt)
+            .end((err, res) => {
+                if (err) { reject(err); }
+                try {
+                    resolve(res.body.trans_result.data[0].dst);
+                } catch (catErr) {
+                    reject(catErr);
+                }
+            });
+    });
+}
+
+function formatSug(sugs) {
+    return sugs.map(s => `${s.k}:${s.v}${os.EOL}`).join('');
+}
+
+function makeTransOpt(from, to, words) {
+    if (from === to) { from = 'auto'; }
+    return {
+        from,
+        to,
+        query: words,
+        transtype: 'realtime',
+        simple_means_flag: 3,
+    };
+}
+
+function printBoxLogUpdate(str, padding = 0, margin = 0, borderStyle = 'double') {
+    logUpdate(
+        boxen(
+            str,
+            {
+                padding,
+                margin,
+                borderStyle,
+            },
+        ),
+    );
+}
 
 module.exports = {
     detectlang,
@@ -11,79 +89,3 @@ module.exports = {
     formatSug,
     suggestTrans,
 };
-
-function _reqTo(url, opt) {
-    return request
-            .post(url)
-            .set('Content-Type', 'application/json')
-            .type('form')
-            .send(opt);
-}
-
-function detectlang(detectword) {
-    return new Promise((resolve, reject) => {
-        _reqTo('http://fanyi.baidu.com/langdetect', {query: detectword})
-            .end((err, res) => {
-                if (err) { return reject(err); }
-                try {
-                    resolve(res.body.lan)
-                } catch(err) {
-                    reject(err);
-                }
-            });
-    });
-}
-
-function suggestTrans(needSubWords) {
-    return new Promise((resolve, reject) => {
-        _reqTo('http://fanyi.baidu.com/sug', {kw: needSubWords})
-            .end((err, res) => {
-                if (err) { return reject(err); }
-                try {
-                    resolve(res.body.data)
-                } catch(err) {
-                    reject(err);
-                }
-            });
-    })
-}
-
-function translate(reqOpt) {
-    return new Promise((resolve, reject) => {
-        _reqTo('http://fanyi.baidu.com/v2transapi', reqOpt)
-            .end((err, res) => {
-                if (err) { return reject(err); }
-                try {
-                    resolve(res.body.trans_result.data[0].dst);
-                } catch(err) {
-                    reject(err);
-                }
-            });
-    });
-}
-
-function formatSug(sugs) {
-    return sugs.map(s => {
-        return `${s.k}:${s.v}${os.EOL}`
-    }).join('');
-}
-
-function makeTransOpt(from, to, words) {
-    if (from === to) { from = 'auto'; }
-    return {
-        from: from,
-        to: to,
-        query: words,
-        transtype: "realtime",
-        simple_means_flag: 3,
-    };
-}
-
-function printBoxLogUpdate(str, padding=0, margin=0, borderStyle='double') {
-    logUpdate(
-        boxen(
-            str,
-            {padding: padding, margin: margin, borderStyle: borderStyle}
-        )
-    );
-}
--- src/index.js
+++ src/index.js
@@ -1,88 +1,84 @@
 #!/usr/bin/env node
 
-const os            = require('os');
-const program       = require('commander');
-const chalk         = require('chalk');
-const helper        = require('./helper');
-const ora           = require('ora');
-const clipboard     = require('clipboardy');
+const os = require('os');
+const program = require('commander');
+const chalk = require('chalk');
+const helper = require('./helper');
+const ora = require('ora');
+const clipboard = require('clipboardy');
 
-let langMap = {
-    ara: "阿拉伯语",
-    auto: "自动检测",
-    bul: "保加利亚语",
-    cht: "繁体中文",
-    cs: "捷克语",
-    dan: "丹麦语",
-    de: "德语",
-    el: "希腊语",
-    en: "英语",
-    est: "爱沙尼亚语",
-    fin: "芬兰语",
-    fra: "法语",
-    hu: "匈牙利语",
-    it: "意大利语",
-    jp: "日语",
-    kor: "韩语",
-    nl: "荷兰语",
-    pl: "波兰语",
-    pt: "葡萄牙语",
-    rom: "罗马尼亚语",
-    ru: "俄语",
-    slo: "斯洛文尼亚语",
-    spa: "西班牙语",
-    swe: "瑞典语",
-    th: "泰语",
-    vie: "越南语",
-    wyw: "文言文",
-    yue: "粤语",
-    zh: "中文"
-}
-
-let spinner = ora({
+const spinner = ora({
     text: 'Translating...',
     color: 'blue',
 });
 
-function trans(words, to='en') {
+// let langMap = {
+//     ara: '阿拉伯语',
+//     auto: '自动检测',
+//     bul: '保加利亚语',
+//     cht: '繁体中文',
+//     cs: '捷克语',
+//     dan: '丹麦语',
+//     de: '德语',
+//     el: '希腊语',
+//     en: '英语',
+//     est: '爱沙尼亚语',
+//     fin: '芬兰语',
+//     fra: '法语',
+//     hu: '匈牙利语',
+//     it: '意大利语',
+//     jp: '日语',
+//     kor: '韩语',
+//     nl: '荷兰语',
+//     pl: '波兰语',
+//     pt: '葡萄牙语',
+//     rom: '罗马尼亚语',
+//     ru: '俄语',
+//     slo: '斯洛文尼亚语',
+//     spa: '西班牙语',
+//     swe: '瑞典语',
+//     th: '泰语',
+//     vie: '越南语',
+//     wyw: '文言文',
+//     yue: '粤语',
+//     zh: '中文'
+// };
+
+
+function trans(words, to = 'en') {
     return new Promise((resolve, reject) => {
         helper.detectlang(words)
-            .then(from => {
-                return helper.makeTransOpt(from, to, words);
-            })
-            .then(transOpt => {
-                resolve(helper.translate(transOpt));
-            })
-            .catch(err => {
-                reject(err);
-            });
+            .then(from => helper.makeTransOpt(from, to, words))
+            .then(transOpt => resolve(helper.translate(transOpt)))
+            .catch(err => reject(err));
     });
 }
 
 function transSug(words) {
     return new Promise((resolve, reject) => {
         helper.suggestTrans(words)
-            .then(data => {
-                if (Array.isArray(data)) { resolve(data); }
-                else { resolve([]); }
+            .then((data) => {
+                if (Array.isArray(data)) {
+                    resolve(data);
+                } else {
+                    resolve([]);
+                }
             })
-            .catch(err => {
-                reject(err);
-            });
+            .catch(err => reject(err));
     });
 }
 
-function runTrans(searchWords, transTo='zh') {
+function runTrans(searchWords, transTo = 'zh') {
     spinner.start();
     Promise.all([trans(searchWords, transTo), transSug(searchWords)])
-        .then(results => {
+        .then((results) => {
             spinner.stop();
             helper.printBoxLogUpdate(
-                chalk.blue(`翻译结果: ${results[0]}${os.EOL+os.EOL}建议: ${os.EOL}${helper.formatSug(results[1])}`)
+                chalk.blue(`翻译结果: ${results[0]}${os.EOL + os.EOL}建议: ${os.EOL}${helper.formatSug(results[1])}`),
             );
             process.exit(0);
         })
-        .catch(err => {
+        .catch((err) => {
             spinner.stop();
             helper.printBoxLogUpdate(chalk.bold.red(err));
             process.exit(1);
@@ -93,16 +89,12 @@ program
     .version('0.0.1')
     .arguments('[words...]')
     .option('-t, --transto <lang>', 'translate to particular language')
-    .action(function (words) {
-        let searchWords = words.join(' ');
-        runTrans(searchWords, program.transto);
-    });
+    .action(words => runTrans(words.join(' '), program.transto));
 
 
 program.parse(process.argv);
 
 if (!process.argv.slice(2).length) {
-    let searchWords = clipboard.readSync();
-    runTrans(searchWords);
+    runTrans(clipboard.readSync());
 }
 
