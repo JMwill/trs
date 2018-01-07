9df018a6922e4038e8cc5903b6e8636c3a77ca4d
JMwill <787505998@qq.com>
2018-01-08 00:16:40 +0800

    修复翻译错误，TODO: 获取翻译结果链接请求错误修复

--- src/index.js
+++ src/index.js
@@ -1,117 +1,166 @@
 #!/usr/bin/env node
 
-const os = require('os');
-const program = require('commander');
-const chalk = require('chalk');
-const logUpdate = require('log-update');
-const helper = require('./helper');
-const ora = require('ora');
-const clipboard = require('clipboardy');
-const table = require('table');
+const program = require('commander')
+const chalk = require('chalk')
+const logUpdate = require('log-update')
+const ora = require('ora')
+const clipboard = require('clipboardy')
+const table = require('table')
+const request = require('superagent')
+const boxen = require('boxen')
+const config = require('./config')
 
-const spinner = ora({
-    text: 'Translating...',
-    color: 'blue',
-});
+const spinner = ora(config.spinner)
 
-let langMap = {
-    ara: '阿拉伯语',
-    auto: '自动检测',
-    bul: '保加利亚语',
-    cht: '繁体中文',
-    cs: '捷克语',
-    dan: '丹麦语',
-    de: '德语',
-    el: '希腊语',
-    en: '英语',
-    est: '爱沙尼亚语',
-    fin: '芬兰语',
-    fra: '法语',
-    hu: '匈牙利语',
-    it: '意大利语',
-    jp: '日语',
-    kor: '韩语',
-    nl: '荷兰语',
-    pl: '波兰语',
-    pt: '葡萄牙语',
-    rom: '罗马尼亚语',
-    ru: '俄语',
-    slo: '斯洛文尼亚语',
-    spa: '西班牙语',
-    swe: '瑞典语',
-    th: '泰语',
-    vie: '越南语',
-    wyw: '文言文',
-    yue: '粤语',
-    zh: '中文',
-};
-
-
-async function trans(words, to = 'en') {
-    try {
-        let lang = await helper.detectlang(words);
-        let transOpt = helper.makeTransOpt(lang, to, words);
-        let transResult = await helper.translate(transOpt);
-        return transResult;
-    } catch (err) {
-        throw new Error(`trans function got an error: ${err}`);
-    }
+async function reqTo(url, opt) {
+  try {
+    let res = await request
+      .post(url)
+      .set('Content-Type', 'application/json')
+      .type('form')
+      .send(opt)
+    return res
+  } catch (err) {
+    throw new Error(config.errTmpl('reqTo', err))
+  }
 }
 
-async function transSug(words) {
-    try {
-        let sugTransResult = await helper.suggestTrans(words);
-        if (!Array.isArray(sugTransResult)) { sugTransResult = []; }
-        return sugTransResult;
-    } catch (err) {
-        throw new Error(`transSub function got an error: ${err}`);
-    }
+async function detectlang(word) {
+  try {
+    let res = await reqTo(`${config.URLs[config.env]}/langdetect`, { query: word })
+    return res.body.lan
+  } catch (err) {
+    throw new Error(config.errTmpl('detectlang', err))
+  }
 }
 
-async function runTrans(searchWords, transTo = 'zh') {
-    spinner.start();
-    try {
-        let transResult = await trans(searchWords, transTo);
-        let sugTransResult = await transSug(searchWords);
-        spinner.stop();
-
-        transResult = `翻译结果: ${transResult}${os.EOL + os.EOL}建议: ${os.EOL}${helper.formatSug(sugTransResult)}`;
-        logUpdate(chalk.blue(helper.doubleBoxen(transResult)));
-        process.exit(0);
-    } catch (err) {
-        spinner.stop();
-        logUpdate(chalk.bold.red(helper.doubleBoxen(err)));
-        process.exit(1);
-    }
+async function getSuggestion(word) {
+  try {
+    let res = await reqTo(`${config.URLs[config.env]}/sug`, { kw: word })
+    return res.body.data || []
+  } catch (err) {
+    throw new Error(config.errTmpl('getSuggestion', err))
+  }
 }
 
-program
+async function getTranslate(postData) {
+  try {
+    let res = await reqTo(`${config.URLs[config.env]}/v2transapi`, postData)
+    return res.body.trans_result || {}
+  } catch (err) {
+    throw new Error(config.errTmpl('getTranslate', err))
+  }
+}
+
+/**
+ * @param {String} from
+ * @param {String} to
+ * @param {String} words
+ * @returns Object
+ */
+function makeTransOpt(from, to, words) {
+  if (from === to) { from = 'auto' }
+  return {
+    from,
+    to,
+    query: words,
+    transtype: 'realtime',
+    simple_means_flag: 3,
+  }
+}
+
+function generateTmplData(suggestion, transResult) {
+  let data = transResult.data || []
+  return {
+    source: data.src || '',
+    result: data.dst || '',
+    suggestion,
+    details: {
+      百度: `http://fanyi.baidu.com/#${data.from}/${data.to}/${data.src}`,
+      谷歌: `https://translate.google.com/#${data.to}/${data.to}/${data.src}`,
+    },
+  }
+}
+
+/**
+ * @param {String} str
+ * @returns String
+ */
+function doubleBoxen(str) {
+  return boxen(str, { borderStyle: 'double' })
+}
+
+async function translate(words, to = config.defaultTransTo) {
+  try {
+    let lang = await detectlang(words)
+    let transOpt = makeTransOpt(lang, to, words)
+    let result = await getTranslate(transOpt)
+    return result
+  } catch (err) {
+    throw new Error(config.errTmpl('translate', err))
+  }
+}
+
+async function runTrs(searchWords, transTo = 'zh') {
+  spinner.start()
+  try {
+    let transResult = await translate(searchWords, transTo)
+    let suggestion = await getSuggestion(searchWords)
+    let data = generateTmplData(suggestion, transResult)
+    spinner.stop()
+
+    logUpdate(chalk.blue(doubleBoxen(config.resultTmpl(data))))
+    process.exit(0)
+  } catch (err) {
+    spinner.stop()
+    logUpdate(chalk.bold.red(doubleBoxen(err)))
+    process.exit(1)
+  }
+}
+
+function main() {
+  program
     .version('0.0.1')
     .arguments('[words...]')
     .option('-t, --transto <lang>', 'translate to particular language')
-    .action(words => runTrans(words.join(' '), program.transto));
+    .action(words => runTrs(words.join(' '), program.transto))
 
-program.on('--help', () => {
+  program.on('--help', () => {
     let data = [
-        ['language', '<lang>', 'language', '<lang>'],
-    ];
-    let langMapEntrys = Object.entries(langMap);
-    let perLineItems = 2;
+      ['language', '<lang>', 'language', '<lang>'],
+    ]
+    let langMapEntrys = Object.entries(config.langMap)
+    let perLineItems = 2
     for (let i = 0, l = Math.floor(langMapEntrys.length / perLineItems); i < l; i += 1) {
-        let tableRow = [];
-        langMapEntrys.slice(i * perLineItems, (i + 1) * perLineItems)
-            .forEach((e) => {
-                [e[0], e[1]] = [e[1], e[0]];
-                tableRow = tableRow.concat(e);
-            });
-        data.push(tableRow);
+      let tableRow = []
+      langMapEntrys.slice(i * perLineItems, (i + 1) * perLineItems)
+        .forEach((e) => {
+          [e[0], e[1]] = [e[1], e[0]]
+          tableRow = tableRow.concat(e)
+        })
+      data.push(tableRow)
     }
-    console.log(table.table(data));
-});
+    console.log(table.table(data))
+  })
 
-program.parse(process.argv);
+  program.parse(process.argv)
 
-if (!process.argv.slice(2).length) {
-    runTrans(clipboard.readSync());
+  if (!process.argv.slice(2).length) {
+    runTrs(clipboard.readSync())
+  }
 }
 
+if (require.main === module) {
+  main()
+} else {
+// export for test
+  module.exports = {
+    detectlang,
+    getTranslate,
+    makeTransOpt,
+    doubleBoxen,
+    getSuggestion,
+    runTrs,
+    translate,
+  }
+}
